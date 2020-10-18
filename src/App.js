7c33176ac700c229140e8228456d9b73ce5bb83c
yyoungeun <1150amy@naver.com>
2020-10-18 23:03:01 +0900

    commit, useReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import Counter from './Counter';
+
+import Info from './Info';
 
 const App = () => {
-  return <Counter/>
+  
+  return <Info/>
+  
+  // const [visible, setVisible] = useState(false);
+  // return (
+  //   <div>
+  //     <button onClick={() => {
+  //       setVisible(!visible);
+  //     }}
+  //     >
+  //       {visible ? '숨기기' : '보이기'}
+  //     </button>
+  //     <hr/>
+  //     {visible && <Info />}
+  //   </div>
+  // );
 };
 
 
